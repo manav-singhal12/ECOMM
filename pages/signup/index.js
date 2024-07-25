511e383d3986001374148c405cdfa6ef992a7739
manav-singhal12 <manavsinghal165@gmail.com>
2024-07-26 00:14:17 +0530

    p

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -131,7 +131,7 @@ const Signup = () => {
               className="border-pink-400 border-2 mb-8 p-2 font-semibold text-lg text-white cursor-pointer bg-customPink rounded-xl"
             />
           </form>
-          <div className="border-t-2 border-black opacity-50 w-full max-w-md mx-auto text-center mb-2"></div>
+          {/* <div className="border-t-2 border-black opacity-50 w-full max-w-md mx-auto text-center mb-2"></div>
           <div className="mx-auto p-2 text-xl mb-3 pb-3 text-center">OR</div>
           <div className="flex flex-col gap-3 justify-center items-center w-full max-w-md">
             <button
@@ -142,7 +142,7 @@ const Signup = () => {
               <svg xmlns="http://www.w3.org/2000/svg" width={16} viewBox="0 0 488 512"><path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" /></svg>
               Continue with Google
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
