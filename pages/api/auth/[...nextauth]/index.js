fdeb2ec9fc33db12f8f6d8fe3deac33f06f618dc
manav-singhal12 <manavsinghal165@gmail.com>
2024-07-26 00:09:46 +0530

    p

--- pages/api/auth/[...nextauth]/index.js
+++ pages/api/auth/[...nextauth]/index.js
@@ -2,7 +2,7 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import connectDB from '@/db/connectdb';
-import User from '../../../models/User';
+import User from '@/models/User';
 import { verifyPassword } from '@/db/auth';
 
 export default NextAuth({
