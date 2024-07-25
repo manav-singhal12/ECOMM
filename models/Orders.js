6ef96953671300da8d41e2653f4f1551e0d78efe
manav-singhal12 <manavsinghal165@gmail.com>
2024-07-25 22:19:52 +0530

    t'

--- models/Orders.js
+++ models/Orders.js
@@ -1,20 +1,20 @@
-// models/Order.js
-import mongoose from 'mongoose';
+// // models/Order.js
+// import mongoose from 'mongoose';
 
-const OrderSchema = new mongoose.Schema({
-    user_email:{type:String,required:true},
-    product_id: { type: Number, required: true },
-    product_name: { type: String },
-    poduct_price: { type: Number },
-    poduct_quantity: { type: Number },
-    size: { type: String },
-    variant: { type: String },
-    image:{type:String},
-    poduct_total: { type: Number },
+// const OrderSchema = new mongoose.Schema({
+//     user_email:{type:String,required:true},
+//     product_id: { type: Number, required: true },
+//     product_name: { type: String },
+//     poduct_price: { type: Number },
+//     poduct_quantity: { type: Number },
+//     size: { type: String },
+//     variant: { type: String },
+//     image:{type:String},
+//     poduct_total: { type: Number },
     
-});
+// });
 
-const Order = mongoose.models.Order || mongoose.model('Order', OrderSchema);
+// const Order = mongoose.models.Order || mongoose.model('Order', OrderSchema);
 
-export default Order;
+// export default Order;
   
